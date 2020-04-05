33bfbeea0bb00ff2bd671538e7b80621b2bdc014
Evan St. Paul <evanstpaul16@augustana.edu>
2020-04-05 13:44:34 -0500

    security update

--- src/firebase/base.js
+++ src/firebase/base.js
@@ -2,14 +2,14 @@ import firebase from '../../node_modules/firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey            : 'AIzaSyAWmhqIZjb2G7mQqSEJ505DKUJV1EXvJgQ',
-    authDomain        : 'react-todo-list-ef6b2.firebaseapp.com',
-    databaseURL       : 'https://react-todo-list-ef6b2.firebaseio.com',
-    projectId         : 'react-todo-list-ef6b2',
-    storageBucket     : 'react-todo-list-ef6b2.appspot.com',
-    messagingSenderId : '914343295567',
-    appId             : '1:914343295567:web:9d45d2554fa55b43b6c83a',
-    measurementId     : 'G-2FQ529XN5C'
+    apiKey            : process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain        : process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL       : process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId         : process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket     : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId : process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId             : process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId     : process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
